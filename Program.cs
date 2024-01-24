526c2a118d64da124227fb807856ba297851e183
suyeol <suyeol@SUYEOL5544>
2024-01-23 17:33:35 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Mission_2;
+
+internal class Program
+
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.Write("How many dice rolls would you like to simulate?");
+        int rolls = int.Parse(System.Console.ReadLine());
+
+        RollingDice rd = new RollingDice();
+        int[] results = rd.RollDice(rolls);
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine($"Total number of rolls = {rolls}.");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            System.Console.Write($"{i}: ");
+            printGraph(results[i - 2], rolls);
+        }
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+
+    static void printGraph(int count, int totalRolls)
+    {
+        int percentage = (count * 100) / totalRolls;
+        for (int i = 0; i < percentage; i++)
+        {
+            System.Console.Write("*");
+        }
+        System.Console.WriteLine();
+    }
+}
+
---
+++ RollingDice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    internal class RollingDice
+    {
+        public int[] RollDice(int rollTotal)
+        {
+            int[] sums = new int[11];
+            Random random = new Random();
+
+            for (int i = 0; i < rollTotal; i++)
+            {
+                int roll1 = random.Next(1, 7);
+                int roll2 = random.Next(1, 7);
+                int sum = roll1 + roll2;
+                sums[sum - 2]++; // Increment the count for this sum
+            }
+
+            return sums;
+        }
+    }
+}
